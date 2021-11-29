31d91e2f5b4b975d87ddbb341cfb24f6c961a839
ttchi98 <ttchi.29041998@gmail.com>
2021-11-29 16:26:41 +0700

    update

---
+++ core/Label.js
@@ -0,0 +1,26 @@
+import { Node } from "./Node.js";
+export class Label extends Node {
+  constructor(string) {
+    super();
+    this._string = string || "";
+    this.string = this._string;
+    this.view.style.color = "black";
+    this.view.style.fontSize = "30px";
+    this.view.style.position = "absolute";
+  }
+
+  get string() {
+    return this._string;
+  }
+
+  set string(value) {
+    this._string = value;
+    this.view.innerHTML = this._string;
+  }
+  get showLabel() {
+    this.view.style.visibility = "visible";
+  }
+  hideLabel() {
+    this.view.style.visibility = "hidden";
+  }
+}
